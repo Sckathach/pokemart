adad225c52c45ceec58493fee6c210a49f25ea23
Sckathach <sckathach@hackademint.org>
2023-10-01 21:56:36 +0200

    Basic controller for Plushies

--- src/Controller/PlushController.php
+++ src/Controller/PlushController.php
@@ -48,5 +48,55 @@ class PlushController extends AbstractController
                 <h1>Liste des peluches</h1>
                 <ul>
         ';
+
+        $entityManager = $doctrine->getManager();
+        $plushies = $entityManager->getRepository(Plush::class)->findAll();
+        foreach($plushies as $plush) {
+            $url = $this->generateUrl(
+                'plush_show',
+                ['id' => $plush->getId()]
+            );
+            $htmlpage .= '<li><a href="' . $url . '">' . $plush->getName() . '</a></li>';
+        }
+        $htmlpage .= '</ul></body></html>';
+
+        return new Response(
+            $htmlpage,
+            Response::HTTP_OK,
+            array('content-type' => 'text/html')
+        );
+    }
+
+    #[Route('/{id}', name: 'plush_show', requirements: ['id' => '\d+'], methods: ['GET'])]
+    public function showAction(Plush $plush): Response
+    {
+        $htmlpage = '
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <meta charset="UTF-8">
+                <title>'.$plush->getName().'</title>
+            </head>
+            <body>
+                <h2>Détails de la peluche</h2>
+                <ul>
+                <dl>
+                    <dt>Peluche</dt>
+                    <dd>'.$plush->getName().'</dd>
+                    <dt>Prix</dt>
+                    <dd>'.$plush->getPrice().'</dd>
+                    <dt>Height</dt>
+                    <dd>'.$plush->getHeight().'</dd>
+                    <dt>Génération</dt>
+                    <dd>'.$plush->getGeneration().'</dd>
+                    <dt>Note</dt>
+                    <dd>'.$plush->getNote().'</dd>
+        ';
+
+        return new Response(
+            $htmlpage,
+            Response::HTTP_OK,
+            array('content-type' => 'text/html')
+        );
     }
 }
