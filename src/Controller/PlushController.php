485a57f5b24d63a45224a9fd24fc11bf32bf2d65
Sckathach <sckathach@hackademint.org>
2023-10-07 21:04:11 +0200

    New colors

--- src/Controller/PlushController.php
+++ src/Controller/PlushController.php
@@ -4,9 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Plush;
 use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/plush')]
 class PlushController extends AbstractController
